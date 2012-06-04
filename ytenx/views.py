5a2316dfb8734012fe5d78cf69cf4547f25fe426
BYVoid <byvoid.kcp@gmail.com>
2012-06-04 17:14:23 +0800

    添加多字查找功能。

--- ytenx/views.py
+++ ytenx/views.py
@@ -11,11 +11,17 @@ def about_page(request):
   return render_to_response('about.html')
 
 def zim(request):
-  dzih = request.GET.get('dzih')
+  dzih_pieux = request.GET.get('dzih')
+  if len(dzih_pieux) > 8:
+    dzih_pieux = dzih_pieux[0:8]
+  
+  dzih_liet = []
+  for dzih in dzih_pieux:
+    dzih_liet.append(dzih)
   
   dzih_list = {
-    'kyonh': KyonhDzih.objects.filter(dzih = dzih),
-    'tcyts': TcytsDzih.objects.filter(dzih = dzih),
+    'kyonh': KyonhDzih.objects.filter(dzih__in = dzih_liet).order_by('ziox'),
+    'tcyts': TcytsDzih.objects.filter(dzih__in = dzih_liet).order_by('ziox'),
   }
   
   return render_to_response('zim.html', {
