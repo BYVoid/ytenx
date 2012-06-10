6f35bca7c898ffd4dcb1d9c3b5b66ec321fd7430
BYVoid <byvoid.kcp@gmail.com>
2012-06-10 19:46:12 +0800

    增加分韻撮要書影導航

--- ytenx/pyonh/models/Cio.py
+++ ytenx/pyonh/models/Cio.py
@@ -18,3 +18,25 @@ class Cio(models.Model):
     
   def dzih(self):
     return self.dzih_set.all().order_by('ziox')
+    
+  def urls(self):
+    base_path = '/pyonh/cio/%d/%d/'
+    max_jep = {
+      1: 105,
+      2: 97,
+    }
+    urls = {
+      'current': base_path % (self.kyenh, self.jep),
+      'first': base_path % (self.kyenh, 1),
+      'last': base_path % (self.kyenh, max_jep[self.kyenh]),
+    }
+    if self.jep > 1:
+      urls['previous'] = base_path % (self.kyenh, self.jep - 1)
+    elif self.kyenh > 1:
+      urls['previous'] = base_path % (self.kyenh - 1, max_jep[self.kyenh - 1])
+    if self.jep < max_jep[self.kyenh]:
+      urls['next'] = base_path % (self.kyenh, self.jep + 1)
+    elif self.kyenh < 5:
+      urls['next'] = base_path % (self.kyenh + 1, 1)
+    
+    return urls
