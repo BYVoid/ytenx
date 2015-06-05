5c72bb213ebc19216c6f0ceb2f7bb31c7b88a8d5
Awc Cho <orancho@AwcdeMacBook-Pro.local>
2015-06-05 16:29:19 +0800

    fix something

--- ytenx/tcenghyonhtsen/urls.py
+++ ytenx/tcenghyonhtsen/urls.py
@@ -2,7 +2,6 @@
 from django.conf.urls import patterns
 
 urlpatterns = patterns('',
-  (r'^sync$', 'ytenx.tcenghyonhtsen.sync.sync'),
   (r'^sieux$', 'ytenx.tcenghyonhtsen.views.sieux_yonh_list_page'),
   (r'^sieux/(\d{1,4})/$', 'ytenx.tcenghyonhtsen.views.sieux_yonh_page'),
   (r'^yonhmiuk$', 'ytenx.tcenghyonhtsen.views.yonh_miuk_list_page'),
