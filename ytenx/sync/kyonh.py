a7766af06164a88bcae51fcbb2cd000a806b59bb
hhliow <hxliow@gmail.com>
2018-10-27 22:01:56 -0400

    Update kyonh.py

--- ytenx/sync/kyonh.py
+++ ytenx/sync/kyonh.py
@@ -21,9 +21,9 @@ pyanxTshetMap = {}
 prengQimMap = {}
 ngixQimMap = {}
 sieuxYonhMap = {}
-gloss_fallback_pattern = ur'(^古文|^俗|上同|^亦同|同上)'
-last_dzih = ''
-last_ngieh = ''
+gloss_fallback_pattern = u'(^古文|^俗|上同|^亦同|同上)'
+last_dzih = ur''
+last_ngieh = ur''
 
 def sync():
   syncCjengMuxPrengQim()
@@ -387,10 +387,10 @@ def syncDzih():
   def sync(line, num):
     global last_dzih
     global last_ngieh
-    current_dzih = ''
-    current_ngieh = ''
+    current_dzih = ur''
+    current_ngieh = ur''
     current_dzih = line[0]
-    current_ngieh = sub(gloss_fallback_pattern, ur'\1(「' + last_dzih + '」' + last_ngieh + ')' , line[3], 1)
+    current_ngieh = sub(gloss_fallback_pattern, ur'\1(「' + last_dzih + ur'」' + last_ngieh + ur')' , line[3], 1)
     dzih = Dzih(
       ziox = num + 1,
       dzih = current_dzih,
