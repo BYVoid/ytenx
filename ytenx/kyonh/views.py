3bf2ba5026bf1b327e37acf63448136cc9082ce8
hhliow <hxliow@gmail.com>
2018-11-06 18:37:23 -0500

    Update views.py

--- ytenx/kyonh/views.py
+++ ytenx/kyonh/views.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 from django.http import Http404
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.views.decorators.cache import cache_page
 from django.db.models import Q
 from ytenx.helpers.paginator import Paginator
