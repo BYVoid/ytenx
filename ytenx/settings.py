66134d8357a3b1b05eeed84aa3c89dd34eb82eb0
hhliow <hxliow@gmail.com>
2018-11-06 21:32:49 -0500

    Update settings.py

--- ytenx/settings.py
+++ ytenx/settings.py
@@ -105,7 +105,7 @@ TEMPLATE_LOADERS = (
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    #'django.contrib.sessions.middleware.SessionMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
     #'django.contrib.auth.middleware.AuthenticationMiddleware',
     #'django.contrib.messages.middleware.MessageMiddleware',
@@ -135,7 +135,7 @@ INSTALLED_APPS = (
   'ytenx.dciangxkox',
   #'django.contrib.auth',
   #'django.contrib.contenttypes',
-  'django.contrib.sessions',
+  #'django.contrib.sessions',
   #'django.contrib.sites',
   #'django.contrib.messages',
   'django.contrib.staticfiles',
@@ -173,14 +173,3 @@ LOGGING = {
         },
     }
 }
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-     'django.contrib.auth.context_processors.auth',
-     'django.core.context_processors.debug',
-     'django.core.context_processors.i18n',
-     'django.core.context_processors.media',
-     'django.core.context_processors.static',
-     'django.core.context_processors.tz',
-     'django.contrib.messages.context_processors.messages',
-     'django.core.context_processors.request',
-)
