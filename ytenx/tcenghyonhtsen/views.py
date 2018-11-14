02e904ab7cf83c5337ed6cef85f56c20cccd51fe
hhliow <hxliow@gmail.com>
2018-11-13 21:29:56 -0500

    Update views.py

--- ytenx/tcenghyonhtsen/views.py
+++ ytenx/tcenghyonhtsen/views.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.http import Http404
 from django.shortcuts import render
+from django.templatetags.static import static
 from django.views.decorators.cache import cache_page
 from ytenx.helpers.paginator import Paginator
 from django.core.paginator import InvalidPage, EmptyPage
@@ -37,9 +38,9 @@ def sieux_yonh_list_page(request):
 def transcription_legend_page(request):
   def listOfLists(filename, separator):
     #return [line.split(separator) for line in open(staticfiles_storage.path(filename), 'r')]
-    return [line.split(separator) for line in open( settings.STATICFILES_DIRS[0] + filename, 'r')]
+    return [line.split(separator) for line in open( static(filename), 'r')]
   return render(request, 'tcenghyonhtsen/transcription_legend.html', {
-    'ghiunh': listOfLists('/tables/jiek_hiunh_ghiunh.tsv', '\t'),
+    'ghiunh': listOfLists('tables/jiek_hiunh_ghiunh.tsv', '\t'),
     'shieng': listOfLists('ytenx/static/tables/jiek_hiunh_shieng.tsv', '\t'),
     'dewh': listOfLists('ytenx/static/tables/jiek_hiunh_dewh.tsv', '\t'),
   })
