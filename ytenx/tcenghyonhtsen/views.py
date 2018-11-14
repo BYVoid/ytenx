4304c9f05996c70ff6ea8aa2e17d857a57e7fd56
hhliow <hxliow@gmail.com>
2018-11-13 21:20:27 -0500

    Update views.py

--- ytenx/tcenghyonhtsen/views.py
+++ ytenx/tcenghyonhtsen/views.py
@@ -38,7 +38,7 @@ def transcription_legend_page(request):
     #return [line.split(separator) for line in open(staticfiles_storage.path(filename), 'r')]
     return [line.split(separator) for line in open(filename, 'r')]
   return render(request, 'tcenghyonhtsen/transcription_legend.html', {
-    'ghiunh': listOfLists('jiek_hiunh_ghiunh.tsv', '\t'),
+    'ghiunh': listOfLists('static/tables/jiek_hiunh_ghiunh.tsv', '\t'),
     'shieng': listOfLists('ytenx/static/tables/jiek_hiunh_shieng.tsv', '\t'),
     'dewh': listOfLists('ytenx/static/tables/jiek_hiunh_dewh.tsv', '\t'),
   })
